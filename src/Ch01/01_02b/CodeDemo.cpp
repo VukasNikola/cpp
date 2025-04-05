8c7744ad636a0d8947bb3356f914d74796f6d982
Nikola Vukas <110349322+VukasNikola@users.noreply.github.com>
2025-04-05 09:42:14 +0000

    Lesson 1 with comments

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,18 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+#include <iostream> //this is a library
+
+//preprocessor directives dont end with a ;
+
+//main is the function that is executed by defult and must return an integer
+//main may receive nothing or strings that come from the command line
+int main(){
+    //c++ does not require indentation
+    std::cout << "Hi!" << std::endl;
+    //using an object form the iostream file which is a member of the standard library, cout is the object. We are sending to the object using the "<<"
+    std::cout << std::endl << std::endl;
+    return 0; //other values mean error codes... 0 because it returns an integer
+}
+//block of code is contained in {}
+
