53572a02035da80fcab9f72ada6a2d083b52803b
Nikola Vukas <110349322+VukasNikola@users.noreply.github.com>
2025-04-05 10:13:27 +0000

    New file in lesson

---
+++ src/Ch01/01_02e/statement_expression.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+int main(){
+
+    int a = 5; //declaration statement
+    int b = 3;
+    int c = a + b; //declaration statement with expression
+
+    if (c > 5){ //conditional statement with an expression
+        std::cout << "c is greater than 5"; //output statement
+    }
+
+    return 0; //return statement
+}
